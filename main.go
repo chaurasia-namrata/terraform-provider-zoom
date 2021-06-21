ed3a05d11106453d42d3cc5d685411b7b00b324c
Namrata Chaurasia <50015194+chaurasia-namrata@users.noreply.github.com>
2021-06-21 14:55:33 +0530

    Update main.go

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"terraform-provider-zoom/zoom"
+	"terraform-provider-zoomus/zoomus"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 )
@@ -9,7 +9,7 @@ import (
 func main() {
 	plugin.Serve(&plugin.ServeOpts{
 		ProviderFunc: func() *schema.Provider {
-			return zoom.Provider()
+			return zoomus.Provider()
 		},
 	})
 }
